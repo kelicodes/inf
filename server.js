8e42ffd64714667966688c151fb58e6092bd44a1
unknown <you@example.com>
2026-04-29 02:30:46 +0300

    p

--- server.js
+++ server.js
@@ -38,7 +38,8 @@ app.use(
       "http://localhost:5173", // local frontend
       "https://infillwear.vercel.app", // deployed frontend
       "https://infillmade.vercel.app",
-      "https://beoapparel.vercel.app"
+      "https://beoapparel.vercel.app",
+      "https://beoadmin.vercel.app/"
       
     ],
     credentials: true, // allow cookies
